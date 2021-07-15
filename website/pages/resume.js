831e298a50d2482c5a69ceb5cb161ff4f6bc5502
Caio Coelho <caioz.co@gmail.com>
2021-07-15 19:15:42 -0400

    Update resume image size

--- website/pages/resume.js
+++ website/pages/resume.js
@@ -22,8 +22,8 @@ export default function Home() {
         <Image
           src="/Caio_C_Coelho_Resume.png"
           alt={name}
-          width="1656"
-          height="2342"
+          width="1000"
+          height="1600"
           className="rounded-sm -mt-3"
           layout="responsive"
           quality={100}
